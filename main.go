1f271c3c15c98dbcb989b61499461934150a1589
chengbin_liu <chengbin_liu@hotmail.com>
2019-05-17 00:34:40 +0800

    //init work

--- main.go
+++ main.go
@@ -1,26 +1,35 @@
 package main
 
 import (
+	"github.com/hxlb/sso/handler"
+	"github.com/hxlb/sso/proto/example"
+	"github.com/hxlb/sso/subscriber"
 	"github.com/micro/go-log"
 	"github.com/micro/go-micro"
-	"sso/handler"
-	"sso/subscriber"
-
-	example "sso/proto/example"
+	"github.com/micro/go-micro/registry"
+	"github.com/micro/go-micro/registry/consul"
+	"time"
 )
 
 func main() {
+	//
+	reg := consul.NewRegistry(
+		registry.Addrs("127.0.0.1:8500"),
+	)
 	// New Service
 	service := micro.NewService(
 		micro.Name("com.hxlb.srv.sso"),
 		micro.Version("latest"),
+		micro.Registry(reg),
+		micro.RegisterTTL(time.Second*30),
+		micro.RegisterInterval(time.Second*15),
 	)
 
 	// Initialise service
 	service.Init()
 
 	// Register Handler
-	example.RegisterExampleHandler(service.Server(), new(handler.Example))
+	com_hxlb_srv_sso.RegisterExampleHandler(service.Server(), new(handler.Example))
 
 	// Register Struct as Subscriber
 	micro.RegisterSubscriber("com.hxlb.srv.sso", service.Server(), new(subscriber.Example))
--- subscriber/example.go
+++ subscriber/example.go
@@ -2,19 +2,18 @@ package subscriber
 
 import (
 	"context"
+	"github.com/hxlb/sso/proto/example"
 	"github.com/micro/go-log"
-
-	example "sso/proto/example"
 )
 
 type Example struct{}
 
-func (e *Example) Handle(ctx context.Context, msg *example.Message) error {
+func (e *Example) Handle(ctx context.Context, msg *com_hxlb_srv_sso.Message) error {
 	log.Log("Handler Received message: ", msg.Say)
 	return nil
 }
 
-func Handler(ctx context.Context, msg *example.Message) error {
+func Handler(ctx context.Context, msg *com_hxlb_srv_sso.Message) error {
 	log.Log("Function Received message: ", msg.Say)
 	return nil
 }
